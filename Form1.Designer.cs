66f3b330f6aff228548697667a62867010edc2cf
antonybogun <antonybogun@gmail.com>
2016-08-25 23:23:02 -0400

    Final Version

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿using System.Windows.Forms;
+
+namespace AntonBogunM16_Lab1_Ex2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("SETechnician");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("SET");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("SET-IG");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("HIT");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("EET");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("BMET");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("ICET", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6});
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("School of Engineering Technology and Applied Science", new System.Windows.Forms.TreeNode[] {
+            treeNode7});
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Centennial College", new System.Windows.Forms.TreeNode[] {
+            treeNode8});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node4";
+            treeNode1.Text = "SETechnician";
+            treeNode2.Name = "Node5";
+            treeNode2.Text = "SET";
+            treeNode3.Name = "Node6";
+            treeNode3.Text = "SET-IG";
+            treeNode4.Name = "Node7";
+            treeNode4.Text = "HIT";
+            treeNode5.Name = "Node8";
+            treeNode5.Text = "EET";
+            treeNode6.Name = "Node10";
+            treeNode6.Text = "BMET";
+            treeNode7.Name = "ICET";
+            treeNode7.Text = "ICET";
+            treeNode8.Name = "School of Engineering Technology and Applied Science";
+            treeNode8.Text = "School of Engineering Technology and Applied Science";
+            treeNode9.Name = "Centennial College";
+            treeNode9.Text = "Centennial College";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode9});
+            this.treeView1.Size = new System.Drawing.Size(319, 237);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(337, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(521, 237);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 261);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.MaximumSize = new System.Drawing.Size(886, 300);
+            this.MinimumSize = new System.Drawing.Size(886, 300);
+            this.Name = "Form1";
+            this.Text = "Course Outline";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntonBogunM16_Lab1_Ex2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.Columns.Add("Semester", 80);
+            this.listView1.Columns.Add("1", 70);
+            this.listView1.Columns.Add("2", 70);
+            this.listView1.Columns.Add("3", 70);
+            this.listView1.Columns.Add("4", 70);
+            this.listView1.Columns.Add("5", 70);
+            this.listView1.Columns.Add("6", 70);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            this.listView1.Items.Clear();
+            if (this.treeView1.SelectedNode.Text == "SETechnician")
+            {
+
+                listView1.Items.Add(new ListViewItem(new[] { "SEM1", "COMM171", "COMP100", "COMP120", "COMP213", "GNED219", "MATH175" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM2", "COMP122", "COMP123", "COMP125", "COMP225", "GNED301", "MATH185" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM3", "COMP214", "COMP228", "COMP229", "COMP246", "GNED500", "ENGL253" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM4", "COMP212", "COMP231", "COMP311", "COMP316", "EMPS101", "GNED" }));
+
+            }
+            else if (this.treeView1.SelectedNode.Text == "SET")
+            {
+                listView1.Items.Add(new ListViewItem(new[] { "SEM1", "COMM-170", "COMP-100", "COMP-120", "COMP-213", "GNED-219", "MATH-175" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM2", "COMP-122", "COMP-123", "COMP-125", "COMP-225", "COMP-301", "MATH-185" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM3", "COMP-214", "COMP-228", "COMP-229", "COMP-246", "ENGL-253", "GNED-500" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM4", "CNET-124", "CNET-329", "COMP-212", "COMP-311", "ELECT  ", "MATH-210" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM5", "COMP-231", "COMP-303", "COMP-304", "COMP-306", "COMP-307", "COMP-309" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM6", "CNET-307", "COMP-308", "COMP-313", "COMP-321", "ELECT 2", "EMPS-102" }));
+
+            }
+            else if (this.treeView1.SelectedNode.Text == "SET-IG")
+            {
+                listView1.Items.Add(new ListViewItem(new[] { "SEM1", "COMM-170", "COMP-100", "COMP-120", "COMP-213", "GNED-219", "MATH-175" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM2", "COMP-122", "COMP-123", "COMP-125", "COMP-225", "COMP-301", "COMP-391" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM3", "COMP-214", "COMP-229", "COMP-246", "COMP-305", "GNED-500", "MATH-185" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM4", "COMP-212", "COMP-228", "COMP-311", "ENGL-253", "GNED    ", "MATH-210" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM5", "CNET-124", "CNET-307", "COMP-231", "COMP-304", "COMP-394", "COMP-396" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM6", "CNET-329", "COMP-308", "COMP-313", "COMP-392", "COMP-395", "COMP-397" }));
+
+
+            }
+            else if (this.treeView1.SelectedNode.Text == "HIT")
+            {
+                listView1.Items.Add(new ListViewItem(new[] { "SEM1", "COMM-170", "COMP-100", "COMP-120", "COMP-213", "GNED-219", "MATH-175" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM2", "COMP-122", "COMP-123", "COMP-125", "COMP-225", "COMP-301", "COMP-380" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM3", "COMP-214", "COMP-229", "COMP-246", "COMP-381", "GNED-500", "MATH-185" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM4", "COMP-212", "COMP-228", "COMP-311", "ENGL-253", "GNED    ", "MATH-210" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM5", "COMP-231", "COMP-304", "COMP-309", "COMP-383", "COMP-384", "COMP-387" }));
+                listView1.Items.Add(new ListViewItem(new[] { "SEM6", "CNET-124", "CNET-307", "CNET-329", "COMP-313", "COMP-321", "COMP-382" }));
+            }
+        }
+    }
+}
+
+
+
+
+
+
